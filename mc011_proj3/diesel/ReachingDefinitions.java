cf8f852f6d3d5537d6b40ddf63a50609a914fd55
Franz Glauber Vanderlinde <franzec05@gmail.com>
2009-06-30 10:21:49 -0300

    final RD - VB

--- mc011_proj3/diesel/ReachingDefinitions.java
+++ mc011_proj3/diesel/ReachingDefinitions.java
@@ -1,39 +1,39 @@
 package diesel;
-
+ 
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map.Entry;
-
+ 
 import temp.Temp;
 import util.List;
 import flow_graph.FlowGraph;
 import graph.Node;
-
+ 
 public class ReachingDefinitions {
-
+ 
     private FlowGraph graph;
-
+ 
     public Hashtable<Node, HashSet<Node>> in = new Hashtable<Node, HashSet<Node>>();
     public Hashtable<Node, HashSet<Node>> out = new Hashtable<Node, HashSet<Node>>();
     private Hashtable<Node, HashSet<Node>> gen;
     private Hashtable<Node, HashSet<Node>> kill;
     public Hashtable<Temp, HashSet<Node>> defs = new Hashtable<Temp, HashSet<Node>>();
-
+ 
     public ReachingDefinitions (FlowGraph gr) {
-
+ 
         super();
         graph = gr;
-
+ 
         computeDefs();
         computeGenKill();
         computeDFA();
-
+ 
     }
-
+ 
     public void computeDefs() {
-
+ 
         for (List<Node> nodes = graph.nodes(); nodes != null; nodes = nodes.tail) {
             for (List<Temp> aux = graph.def(nodes.head); aux != null; aux = aux.tail) {
                 if (defs != null && defs.containsKey(aux.head))
@@ -46,18 +46,21 @@ public class ReachingDefinitions {
             }
         }
     }
-
+ 
     public void computeGenKill() {
         kill = new Hashtable<Node, HashSet<Node>>();
         gen = new Hashtable<Node, HashSet<Node>>();
-
+ 
         for (List<Node> nodes = graph.nodes(); nodes != null; nodes = nodes.tail) {
             HashSet<Node> k = new HashSet<Node>();
             HashSet<Node> g = new HashSet<Node>();
+
+            // calcula conjunto kill
             for (List<Temp> aux = graph.def(nodes.head); aux != null; aux = aux.tail) {
-                k = (HashSet<Node>)(defs.get(aux.head)).clone();
-                k.remove(nodes.head);                
+                k = (HashSet<Node>)(defs.get(nodes.head)).clone();
+                k.remove(nodes.head);
             }
+            // calcula conjunto gen
             if (graph.def(nodes.head) != null)
                 g.add(nodes.head);
 
@@ -65,44 +68,85 @@ public class ReachingDefinitions {
             gen.put(nodes.head, g);
         }
     }
-
+ 
     public void computeDFA() {
         Hashtable<Node, HashSet<Node>> inl = null;
         Hashtable<Node, HashSet<Node>> outl = null;
-
         inl = new Hashtable<Node, HashSet<Node>>();
         outl = new Hashtable<Node, HashSet<Node>>();
         in = new Hashtable<Node, HashSet<Node>>();
         out = new Hashtable<Node, HashSet<Node>>();
+    	List<Node> inverseNodes;  	
+    	List<Node> iter = null;
 
-        for (List<Node> nodes = graph.nodes(); nodes != null; nodes = nodes.tail) {
-            in.put(nodes.head, new HashSet<Node>());
-            out.put(nodes.head, new HashSet<Node>());
-            inl.put(nodes.head, new HashSet<Node>());
-            outl.put(nodes.head, new HashSet<Node>());
+    	for ( List<Node> instrs = graph.nodes(); instrs != null; instrs = instrs.tail ) {
+    		if (iter == null)
+    			iter = new List<Node>(instrs.head, null);
+    		else
+    			iter = new List<Node>(instrs.head, iter);
         }
-        do {
-            for (List<Node> nodes = graph.nodes(); nodes != null; nodes = nodes.tail) {
-            inl.remove(nodes.head);
-            inl.put(nodes.head, (HashSet<Node>)in.get(nodes.head).clone());
-            outl.remove(nodes.head);
-            outl.put(nodes.head, (HashSet<Node>)out.get(nodes.head).clone());
+    	
+    	inverseNodes = iter;
+    	
+    	// n é o nó do grafo
+    	
+    	// for each n
+    	// in[n] <- {}
+    	// out[n] <- {}
+    	 	
+    	for (List<Node> instrs = inverseNodes; instrs != null; instrs = instrs.tail) {
+            in.put(instrs.head, new HashSet<Node>());
+            out.put(instrs.head, new HashSet<Node>());            
+        }
+    	
+    	// repeat
+    	int hasChanged;
+    	do {    		
+    		// for each n
+        	for (List<Node> instrs = inverseNodes; instrs != null; instrs = instrs.tail) {
+        		Node n = instrs.head;
+        		        		
+        		// in'[n] <- in[n]
+        		inl.put(n, in.get(n));
+        		
+        		// out'[n] <- out[n]in
+        		outl.put(n, out.get(n));
+        		
+        		// in[n] <- use[n] U (out[n] - def[n])
+        		HashSet<Node> tmp = new HashSet<Node>();
+        		tmp.addAll(out.get(n));
+        		tmp.removeAll(kill.get(n));
+        		tmp.addAll(gen.get(n));        		
+        		in.put(n, tmp);
+        		
+        		// out[n] <- U in[s] (s is for all successors of n)
+        		tmp = new HashSet<Node>();
+        		for (List<Node> s = n.succ(); s != null; s = s.tail) {
+        			Node ns = s.head;        			
+        			tmp.addAll(in.get(ns));        			
+        		}
 
-            HashSet<Node> i = new HashSet<Node>();
-
-            for (List<Node> s = nodes.head.pred(); s != null; s = s.tail)
-                i.addAll((HashSet<Node>)out.get(s.head).clone());
-
-            in.remove(nodes.head);
-            in.put(nodes.head, (HashSet<Node>)i.clone());
-
-            HashSet<Node> o = new HashSet<Node>();
-            o.addAll((HashSet<Node>)i.clone());
-            o.removeAll((Collection<Node>)kill.get(nodes.head));
-            o.addAll(gen.get(nodes.head));
-            out.remove(nodes.head);
-            out.put(nodes.head, (HashSet<Node>)o.clone());
+        		out.put(n, tmp);        		
             }
-        } while ((!inl.equals(in)) || (!outl.equals(out)));
+        	
+        	hasChanged = 0;
+        	for ( List<Node> instrs = inverseNodes; instrs != null; instrs = instrs.tail )
+            {
+        		Node n = instrs.head;
+        		
+        		if (in.get(n).equals(inl.get(n)) == false) {
+        			hasChanged = 1;
+        			break;
+        		}
+        		
+        		if (out.get(n).equals(outl.get(n)) == false) {
+        			hasChanged = 1;
+        			break;
+        		}
+            }
+    		
+    	} while (hasChanged==1);
+    	// until in'[n] = in[n] and out'[n] = out[n] for all n
+ 
     }
 }
